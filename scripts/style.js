df34041d2916b9cc3376b4ed9ecdeab405efad3b
Santosh Divate <dsantoshkumarit@gmail.com>
2021-04-18 20:57:47 +0530

    First commit

---
+++ scripts/style.js
@@ -0,0 +1,99 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    //Animate links (index/7) adds increasing delay to eash link using index
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ``;
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+
+    //Burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
+
+//Toggle navbar once link is clicked
+handleAnchorClick = () => {
+  const burger1 = document.querySelector(".burger");
+  const nav1 = document.querySelector(".nav-links");
+  const navLinks1 = document.querySelectorAll(".nav-links li");
+  console.log("in event handler");
+
+  navLinks1.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = ``;
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${
+        index / 7 + 0.5
+      }s`;
+    }
+  });
+  nav1.classList.toggle("nav-active");
+  burger1.classList.toggle("toggle");
+};
+
+//Smart Scrolling
+const nav = document.querySelector("nav");
+const supportPageOffset = window.pageXOffset !== undefined;
+const isCSS1Compat = (document.compatMode || "") === "CSS1Compat";
+
+let previousScrollPosition = 0;
+
+const isScrollingDown = () => {
+  let scrolledPosition = supportPageOffset
+    ? window.pageYOffset
+    : isCSS1Compat
+    ? document.documentElement.scrollTop
+    : document.body.scrollTop;
+  let isScrollDown;
+
+  if (scrolledPosition > previousScrollPosition) {
+    isScrollDown = true;
+  } else {
+    isScrollDown = false;
+  }
+  previousScrollPosition = scrolledPosition;
+  return isScrollDown;
+};
+
+const handleNavScroll = () => {
+  if (isScrollingDown() && !nav.contains(document.activeElement)) {
+    nav.classList.add("scroll-down");
+    nav.classList.remove("scroll-up");
+  } else {
+    nav.classList.add("scroll-up");
+    nav.classList.remove("scroll-down");
+  }
+};
+
+var throttleTimer;
+
+const throttle = (callback, time) => {
+  if (throttleTimer) return;
+
+  throttleTimer = true;
+  setTimeout(() => {
+    callback();
+    throttleTimer = false;
+  }, time);
+};
+
+const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+
+window.addEventListener("scroll", () => {
+  if (mediaQuery && !mediaQuery.matches) {
+    throttle(handleNavScroll, 250);
+  }
+});
